437a40984acec4bd5948ea74044fb88e4819c25a
sam <sfparker91@googlemail.com>
2020-03-02 11:28:44 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ COPY . /app
 WORKDIR /app
 
 RUN pip install -r requirements.txt
+RUN pip install -e /strat_evolve/. 
 
 CMD python run_example.py
---
+++ strat_evolve/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='genetic-bt',
+      version='0.0.1',
+      description='Trading Strategy Genetic Optimiser',
+      author='Sam Parker',
+      packages=find_packages())
